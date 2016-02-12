5823fe55ea668c2a27c9d9c1a5dfd2a45a51d49e
mitchmindtree <mitchell.nordine@gmail.com>
2016-02-13 00:59:23 +1100

    Add section about setting up Ui to 3rd chapter

--- src/guide/chapter_3.rs
+++ src/guide/chapter_3.rs
@@ -157,6 +157,7 @@ The `ups` method comes from the `EventLoop` trait - we'll need to import it too.
 ```
 # extern crate piston_window;
 use piston_window::{EventLoop, PistonWindow, WindowSettings};
+# fn main() {}
 ```
 
 Okydoke, feel free to run this if you haven't already! We should now have an empty window, ripe for
@@ -192,7 +193,162 @@ fn main() {
 
 Now that the window's ready, we can begin setting up our GUI.
 
-Every conrod GUI requires an instance of a `Ui`. The `Ui` can be thought of 
+Every conrod GUI requires an instance of a [`Ui`][Ui]. The `Ui` plays a couple of important roles:
+
+- Manages the state of all widgets and their relationships within an internal graph.
+- Receives and distributes input events to all widgets and manages the capturing of different
+  kinds of input.
+- Owns the `Theme`, used for specifying styling defaults on the occasion that the user does not
+  specify styling themselves.
+- Owns the `GlyphCache`, used for checking the size of text, as well as rendering it to the screen.
+
+The `Ui` is generic over the kinds of character cache it may use. Users may use any character cache
+as long as it implements the [`CharacterCache` trait][CharacterCache].
+
+We're going to use the character cache provided by the `piston_window` crate called `Glyphs`. We
+can import `Glyphs` and write a type alias for our `Ui` type so that we don't have to keep writing
+`Ui<Glyphs>` everywhere:
+
+```
+# extern crate conrod;
+# extern crate piston_window;
+use piston_window::{EventLoop, Glyphs, PistonWindow, WindowSettings};
+
+type Ui = conrod::Ui<Glyphs>;
+# fn main() {}
+```
+
+To construct the `Ui`, we'll first find the path to our font within the assets directory. We'll do
+this just before the event loop begins.
+
+```
+# extern crate find_folder;
+# fn main() {
+let assets = find_folder::Search::ParentsThenKids(3, 3)
+    .for_folder("assets").unwrap();
+let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
+# }
+```
+
+*Note: Be sure to check that this path is exactly where you placed the font.*
+
+We'll use the font path to load the `Glyphs`:
+
+```
+# extern crate conrod;
+# extern crate find_folder;
+# extern crate piston_window;
+# use piston_window::{EventLoop, Glyphs, PistonWindow, WindowSettings};
+# 
+# fn main() {
+# 
+#     // Construct the window.
+#     let window: PistonWindow =
+#         WindowSettings::new("Simple GUI", [800, 600])
+#             .exit_on_esc(true).vsync(true).samples(4).build().unwrap();
+# 
+#     let assets = find_folder::Search::ParentsThenKids(3, 3)
+#         .for_folder("assets").unwrap();
+#     let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
+# 
+let glyphs = Glyphs::new(&font_path, window.factory.borrow().clone());
+# }
+```
+
+*Note: Please ignore the `window.factory.borrow().clone()` argument for now - it is an
+implementation detail of the graphics backend that `piston_window` uses, and is not relevant to
+conrod itself*.
+
+Then we'll first import conrod's [`Theme`][Theme] type
+
+```
+# extern crate conrod;
+use conrod::Theme;
+# fn main() {}
+```
+
+and construct a default `Theme`:
+
+```
+# extern crate conrod;
+# use conrod::Theme;
+# fn main() {
+let theme = Theme::default();
+# }
+```
+
+We'll go into more detail on customising themes in the following chapter. For now, we want to keep
+things simple!
+
+We now have both parts necessary for constructing our `Ui`.
+
+```
+# extern crate conrod;
+# extern crate find_folder;
+# extern crate piston_window;
+# 
+# use piston_window::{EventLoop, Glyphs, PistonWindow, WindowSettings};
+# use conrod::Theme;
+# 
+# type Ui = conrod::Ui<Glyphs>;
+# 
+# fn main() {
+#     return; // This is just so that the tests return before opening the window and looping
+# 
+#     // Construct the window.
+#     let window: PistonWindow =
+#         WindowSettings::new("Simple GUI", [800, 600])
+#             .exit_on_esc(true).vsync(true).samples(4).build().unwrap();
+# 
+#     let assets = find_folder::Search::ParentsThenKids(3, 3)
+#         .for_folder("assets").unwrap();
+#     let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
+#     let theme = Theme::default();
+#     let glyph_cache = Glyphs::new(&font_path, window.factory.borrow().clone());
+let mut ui = Ui::new(glyph_cache.unwrap(), theme);
+# }
+```
+
+
+
+```
+#[macro_use] extern crate conrod;
+extern crate find_folder;
+extern crate piston_window;
+
+use conrod::Theme;
+use piston_window::{EventLoop, Glyphs, PistonWindow, WindowSettings};
+
+type Ui = conrod::Ui<Glyphs>;
+
+fn main() {
+    # return; // This is just so that the tests return before opening the window and looping
+
+    // Construct the window.
+    let window: PistonWindow =
+        WindowSettings::new("Simple GUI", [800, 600])
+            .exit_on_esc(true) // Finish the event loop when `Esc` is pressed
+            .vsync(true) // Turn on vertical sync
+            .samples(4) // Use multi-sample anti-aliasing
+            .build() // Build the `PistonWindow` from these settings.
+            .unwrap(); // Don't worry about handling errors for now.
+
+    // Construct the `Ui`.
+    let assets = find_folder::Search::ParentsThenKids(3, 3)
+        .for_folder("assets").unwrap();
+    let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
+    let theme = Theme::default();
+    let glyph_cache = Glyphs::new(&font_path, window.factory.borrow().clone());
+    let mut ui = Ui::new(glyph_cache.unwrap(), theme);
+
+    // Kick off the event loop, yielding 60 `Update`s per second.
+    for event in window.ups(60) {
+        // This is where we'll respond to each event
+    }
+}
+```
+
+
 
 
 [Getting Started]:      ../chapter_2/index.html                     "Chapter 2: Getting Started"
@@ -200,7 +356,9 @@ Every conrod GUI requires an instance of a `Ui`. The `Ui` can be thought of
 
 [Conrod NotoSans Fonts]:    https://github.com/PistonDevelopers/conrod/tree/master/assets/fonts/NotoSans    "NotoSans"
 
-[Ui]:   ../../struct.Ui.html    "Ui"
+[CharacterCache]:   ../../backend/trait.CharacterCache.html "CharacterCache"
+[Theme]:            ../../struct.Theme.html                 "Theme"
+[Ui]:               ../../struct.Ui.html                    "Ui"
 
 */
 
