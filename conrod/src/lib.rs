fbc701f8eb48e6d471a635af59380e58171a3088
mitchmindtree <mitchell.nordine@gmail.com>
2020-08-02 17:28:27 +0200

    Publish dummy conrod crate to point to conrod-core

---
+++ conrod/src/lib.rs
@@ -0,0 +1,3 @@
+//! In version 0.62.0, `conrod` was split into multiple sub-crates. The `conrod` crate itself is no
+//! longer used. You are most likely looking for
+//! [`conrod_core`](https://crates.io/crates/conrod-core).
